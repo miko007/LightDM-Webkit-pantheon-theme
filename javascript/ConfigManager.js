miko007/LightDM-Webkit-pantheon-theme

commit 908860e9c489ab9927fdb2d83aad38a27a485351

added config system. this resolves background problem

javascript/ConfigManager.js
@@ -1,0 +1,7 @@
+var ConfigManager = function(greeter) {
+	this.greeter	= greeter;
+
+	if (Config.background) {
+		$('body').css('background', "url('" + Config.background + "') no-repeat black");
+	}
+};

.gitignore
@@ -1,3 +1,4 @@
 .idea
 dump.json
 bg.jpg
+config.js

config.sample.js
@@ -1,0 +1,3 @@
+var Config = {
+	background	: "test.jpg"
+};

index.html
@@ -3,11 +3,13 @@
 	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
 	<link rel="stylesheet" type="text/css" href="pantheon.css" />
 	<link rel="stylesheet" type="text/css" href="css/dropdown.min.css" />
+	<script type="text/javascript" src="config.js"></script>
 	<script type="text/javascript" src="javascript/lightdm-mockup.js"></script>
 	<script type="text/javascript" src="javascript/jquery.js"></script>
 	<script type="text/javascript" src="javascript/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="javascript/dropdown.min.js"></script>
 
+	<script type="text/javascript" src="javascript/ConfigManager.js"></script>
 	<script type="text/javascript" src="javascript/UserController.js"></script>
 	<script type="text/javascript" src="javascript/EventListener.js"></script>
 	<script type="text/javascript" src="javascript/GUI.js"></script>

javascript/Greeter.js
@@ -1,4 +1,5 @@
 var Greeter = function() {
+	this.configManager	= new ConfigManager(this);
 	this.eventlistener	= new EventListener(this);
 
 	var $this = this;

pantheon.css
@@ -9,7 +9,7 @@
 }
 
 body {
-	background: url('bg.jpg') no-repeat black;
+	background: url('background.jpg') no-repeat black;
 	background-size: cover;
 	font-family:  "Open Sans", sans-serif;
 	color: white;

commit f2da113daaddcf5f5707d4424453e17f53351888

resolved background size issue caused by 908860e9c489ab9927fdb2d83aad38a27a485351

javascript/ConfigManager.js
@@ -2,6 +2,6 @@ var ConfigManager = function(greeter) {
 	this.greeter	= greeter;
 
 	if (Config.background) {
-		$('body').css('background', "url('" + Config.background + "') no-repeat black");
+		$('body').css('background', "url('" + Config.background + "') no-repeat black").css('background-size', 'cover');
 	}
 };