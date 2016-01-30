miko007/LightDM-Webkit-pantheon-theme

commit f2da113daaddcf5f5707d4424453e17f53351888

resolved background size issue caused by 908860e9c489ab9927fdb2d83aad38a27a485351

javascript/ConfigManager.js
@@ -2,6 +2,6 @@ var ConfigManager = function(greeter) {
 	this.greeter	= greeter;
 
 	if (Config.background) {
-		$('body').css('background', "url('" + Config.background + "') no-repeat black");
+		$('body').css('background', "url('" + Config.background + "') no-repeat black").css('background-size', 'cover');
 	}
 };

commit 023293bffd0ced27913ee229a8f5a2a2e6b01790

resolved not existing config bug, caused by f2da113daaddcf5f5707d4424453e17f53351888

javascript/ConfigManager.js
@@ -1,6 +1,8 @@
 var ConfigManager = function(greeter) {
 	this.greeter	= greeter;
 
+	if (typeof Config == 'undefined')
+		return;
 	if (Config.background) {
 		$('body').css('background', "url('" + Config.background + "') no-repeat black").css('background-size', 'cover');
 	}
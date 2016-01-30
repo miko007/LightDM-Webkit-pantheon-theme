023293bffd0ced27913ee229a8f5a2a2e6b01790
Michael Ochmann <miko007@me.com>
2016-01-30 12:55:43 +0100

    resolved not existing config bug, caused by f2da113daaddcf5f5707d4424453e17f53351888

--- javascript/ConfigManager.js
+++ javascript/ConfigManager.js
@@ -1,6 +1,8 @@
 var ConfigManager = function(greeter) {
 	this.greeter	= greeter;
 
+	if (typeof Config == 'undefined')
+		return;
 	if (Config.background) {
 		$('body').css('background', "url('" + Config.background + "') no-repeat black").css('background-size', 'cover');
 	}
